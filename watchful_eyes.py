e9fc432d3acd059045fca1765e93943fa05cad4d
GhostofBearBryant <89320003+GhostofBearBryant@users.noreply.github.com>
2022-02-28 12:20:38 -0500

    Update watchful_eyes.py

--- watchful_eyes.py
+++ watchful_eyes.py
@@ -2,6 +2,7 @@
 # Watchful Eyes Ban Bot v.2 by u/BuckRowdy 
 
 # Import modules
+# Import PMTW to leave toolbox usernotes
 import praw
 import pmtw
 import time
@@ -20,28 +21,36 @@ sub_name = config.sub_name
 # Number of seconds to sleep between scans
 sleep_seconds = 30
 
-# User Defined nickname for ban reasons. Put what you go by on your subs here. 
-nick_name = 'Bucky'
+# Sub to log bans and admin actions.  Admin removals and bans by this account will be logged in the sub.
+submit_sub = "YOUR_LOG_SUBREDDIT"
+
 
 # Define your ban notes here.  These are for the ban note in the upper right corner of modmail in red.
 # Both fields are editable, but you must use the letters in the first column in your report reason.
-# Report reasons always start with 'ban' followed by the time then the code for the reason.  
+# Report reasons always start with 'ban' followed by the time then the code for the reason.  It's not case-sensitive.
 # A 30 day ban for trolling would read as follows: ban 30 t
-# Permanent bans are input as 0.  So a perm ba for covid misinfo would be ban 0 c
+# Permanent bans are input as 0.  So a perm ban for covid misinfo would be ban 0 c
 
-ban_macros = 		{ 	'c' : 'Covid Misinfo',
-				'v' : 'Violent Content',
-				'r' : 'Racism',
-				't' : 'Trolling',
-				's' : 'Spam',
-				'm' : 'Misinformation'
-			}
+ban_macros = 			{ 	'c' : 'Covid Misinfo',
+							'v' : 'Violent Content',
+							'r' : 'Bigotry/Racism',
+							't' : 'Trolling',
+							's' : 'Spam',
+							'm' : 'Misinformation',
+							'd' : 'Dehumanizing Speech',
+							'n' : 'Nazi',
+							'b' : 'Brigading',
+							'h' : 'Hate Speech'
+						}
 
 
 
 ###  Defines the reddit login function
-#    A refresh token is used so you can maintain 2FA on the bot account.
-def reddit_login():	 
+### A refresh token is used so you can maintain 2FA on the bot account.
+### Login items stored in a config.py file and imported.
+def reddit_login():
+
+	 
 	try:
 		reddit = praw.Reddit(   
 								user_agent = config.user_agent,
@@ -56,21 +65,26 @@ def reddit_login():
 	return reddit
 
 
-### Process ModQueue comments 
-#   Comments and Submissions are processed separately.
-def check_comments(subreddit):
+# Process ModQueue comments and submissions.
+
+def check_modqueue(subreddit):
 	try: 
 		ban_phrase = 'ban'
+
+		###  Process Comments
 		# Prints the ban syntax to the terminal so you can easily reference.
-		print('\nBan syntax:\nban <days> [macro]\n<0> days for perm\n\nMacros:\n[c] Covid\n[v] Violence\n[s] Spam\n[r] Racism\n[t] Trolling\n[m] Misinfo\n\n')
+		print('\nBan syntax:\nban <days> [macro]\n<0> days for perm\n\nReport reasons:\n[c] Covid\n[v] Violence\n[s] Spam\n[r] Racism/Bigotry\n[t] Trolling\n[m] Misinformation\n[d] Dehumanizing speech\n[n] Nazi\n[b] Brigading\n[h] Hate Speech\n\n')
 		for item in subreddit.mod.modqueue(limit=None):
 			# T1 is reddit's code for comment objects.
 			if item.fullname.startswith("t1_") and len(item.mod_reports):
+				item_type = 'Comment'
 				
 				# Ban message field has a 1000 character limit so quoted comments need to be sliced to prevent api errors.
 				comment_slice = f'{item.body}'
-				sliced_comment = comment_slice[:300]
-				item_type = 'Comment'
+				replace_comment = comment_slice[:300]
+
+				# Takes multi line comment and joins it so it can be properly formatted when linked.
+				sliced_comment = ''.join(f'{i}' for i in replace_comment.split('\n\n'))
 				
 				# Set up a ban header, footer, and comment quote.  Gives granular control over these fields.
 				ban_header = f'This **{item_type}** may have fully or partially contributed to your ban:\n\n'
@@ -81,14 +95,15 @@ def check_comments(subreddit):
 				for report_reason, mod_name in item.mod_reports:
 					if ban_phrase in report_reason:
 						items = report_reason.split(" ")
-						ban_duration = items[1]
-						ban_reason = items[2]
-						ban_length = int(ban_duration)
+						ban_length = int(items[1])
+						ban_reason_raw = items[2]
+						ban_reason = ban_reason_raw.lower()
+						#ban_length = int(ban_duration)
 						
 						# Ban note is a field in the /about/banned page as well as in modmail and modlog. 
 						# Here, the ban note is taken from a user defined dictionary. 
 						ban_note = ban_macros[ban_reason]
-						print('Mod report found...')
+						print('\nMod report found...')
 						time.sleep(1)
 						print(f'r/{item.subreddit} | {mod_name}: {report_reason}')
 						 
@@ -96,7 +111,7 @@ def check_comments(subreddit):
 							if items[0] == 'ban':
 								item.mod.remove()
 								item.mod.lock()
-								print(f"REMOVE ITEM {item.fullname}")
+								print(f"REMOVE ITEM {item.fullname} ## {item.permalink}")
 								ban_sub = item.subreddit.display_name
 
 								# Ban length of 0 for permanent bans, any other digit will trigger a temp ban.
@@ -105,11 +120,11 @@ def check_comments(subreddit):
 										ban_note = ban_macros[ban_reason]
 										ban_message = '**'+ban_note+'**\n\n'+ban_header+comment_quote+ban_footer
 										reddit.subreddit(ban_sub).banned.add(
-															f'{item.author}', 
-															ban_reason=f'{ban_note} - {nick_name}', 
-															ban_message=f'{ban_message}', 
-															note=f'{item.permalink}'
-															)										
+																				f'{item.author}', 
+																				ban_reason=f'{ban_note} - {mod_name}', 
+																				ban_message=f'{ban_message}', 
+																				note=f'{item.permalink}'
+																				)										
 										
 										print(f"{mod_name} banned {item.author} permanently.")
 
@@ -117,7 +132,7 @@ def check_comments(subreddit):
 										notes = pmtw.Usernotes(reddit, item.subreddit)
 										n = pmtw.Note(
 														user=f'{item.author}', 
-														note=f'ban - {ban_note}',
+														note=f'{ban_note} - {mod_name}',
 														link=f'{item.permalink}',
 														warning='ban'
 														)
@@ -129,12 +144,12 @@ def check_comments(subreddit):
 										ban_note = ban_macros[ban_reason]
 										ban_message = '**'+ban_note+'**\n\n'+ban_header+comment_quote+ban_footer
 										reddit.subreddit(ban_sub).banned.add(
-															f'{item.author}', 
-															ban_reason=f'{ban_note} - {nick_name}', 
-															duration=ban_length, 
-															ban_message=f'{ban_message}', 
-															note=f'{item.permalink}'
-															)
+																				f'{item.author}', 
+																				ban_reason=f'{ban_note} - {mod_name}', 
+																				duration=ban_length, 
+																				ban_message=f'{ban_message}', 
+																				note=f'{item.permalink}'
+																				)
 
 										print(f"{mod_name} banned {item.author} for {ban_duration} days.")
 										notes = pmtw.Usernotes(reddit, item.subreddit)
@@ -146,17 +161,8 @@ def check_comments(subreddit):
 														)
 										notes.add_note(n)
 										
-								break
-	except Exception as e:
-			print('\t### ERROR - Something went wrong processing comments.')
-			print(e)
-			traceback.print_exc()						
-										
+			###  Process Submissions 
 
-def check_submissions(subreddit):
-	try: 
-		ban_phrase = 'ban'
-		for item in subreddit.mod.modqueue(limit=None):
 			if item.fullname.startswith("t3_") and len(item.mod_reports):
 				notes = pmtw.Usernotes(reddit, item.subreddit)
 				item_type = 'Submission'
@@ -168,7 +174,8 @@ def check_submissions(subreddit):
 					if ban_phrase in report_reason:
 						items = report_reason.split(" ")
 						ban_duration = items[1]
-						ban_reason = items[2]
+						ban_reason_raw = items[2]
+						ban_reason = ban_reason_raw.lower()
 						ban_length = int(ban_duration)
 						ban_note = ban_macros[ban_reason]
 						print('Mod report found...')
@@ -188,17 +195,17 @@ def check_submissions(subreddit):
 										ban_note = ban_macros[ban_reason]
 										ban_message = '**'+ban_note+'**\n\n'+ban_header+post_quote+ban_footer
 										reddit.subreddit(ban_sub).banned.add(
-															f'{item.author}', 
-															ban_reason=f'{ban_note} - {nick_name}', 
-															ban_message=f'{ban_message}', 
-															note=f'{item.permalink}'
-															)
+																				f'{item.author}', 
+																				ban_reason=f'{ban_note} - {mod_name}', 
+																				ban_message=f'{ban_message}', 
+																				note=f'{item.permalink}'
+																				)
 										
 										print(f"{mod_name} banned {item.author} permanently.")
 										notes = pmtw.Usernotes(reddit, item.subreddit)
 										n = pmtw.Note(
 												user=f'{item.author}', 
-												note=f'ban - {ban_note}',
+												note=f'{ban_note} - {mod_name}',
 												link=f'{item.permalink}',
 												warning='ban'
 												)
@@ -209,31 +216,35 @@ def check_submissions(subreddit):
 										ban_note = ban_macros[ban_reason]
 										ban_message = '**'+ban_note+'**\n\n'+ban_header+post_quote+ban_footer
 										reddit.subreddit(ban_sub).banned.add(
-															f'{item.author}', 
-															ban_reason=f'{ban_note} - {nick_name}', 
-															duration=ban_length, 
-															ban_message=f'{ban_message}', 
-															note=f'{item.permalink}'
-															)
+																				f'{item.author}', 
+																				ban_reason=f'{ban_note} - {mod_name}', 
+																				duration=ban_length, 
+																				ban_message=f'{ban_message}', 
+																				note=f'{item.permalink}'
+																				)
 										
 										print(f"{mod_name} banned {item.author} for {ban_duration} days.")
 										notes = pmtw.Usernotes(reddit, item.subreddit)
 										n = pmtw.Note(
 														user=f'{item.author}', 
-														note=f'ban - {ban_note}',
+														note=f'{ban_note} - {mod_name}',
 														link=f'{item.permalink}',
 														warning='ban'
 														)
 										notes.add_note(n)
 										
-								break
+								
 	except Exception as e:
-			print('\t### ERROR - Something went wrong processing submissions.')
+			print('\t### ERROR - Something went wrong processing the modqueue.')
 			print(e)
-			traceback.print_exc()
+			traceback.print_exc()					
+	
 
+
+ 
 ##############################
 # Bot starts here				
+
 
 if __name__ == "__main__":
 
@@ -241,19 +252,19 @@ if __name__ == "__main__":
 			# Connect to reddit and return the object
 			reddit = reddit_login()
 
-			# Connect to the sub(s)
+			# Connect to the sub
 			subreddit = reddit.subreddit(sub_name)
 
 	except Exception as e:
 		print('\t\n### ERROR - Could not connect to reddit.')
 		sys.exit(1) 
 		
+	# Loop the bot
 	while True:
 
 		try:	
-			ban_comments = check_comments(subreddit)
-			ban_posts = check_submissions(subreddit)
-							
+			ban_comments = check_modqueue(subreddit)
+			
 		except Exception as e:
 			print('\t### ERROR - Something went wrong.')
 			print(e)
@@ -261,6 +272,3 @@ if __name__ == "__main__":
 
 		# Loop every X seconds, defined above (1 minute, currently.)
 		time.sleep(sleep_seconds) 
-
-
-
